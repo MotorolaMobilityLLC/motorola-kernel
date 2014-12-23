978771cf8b11ca5b3a02f322058b1d6f2c0cd29d
Chao Yu <chao2.yu@samsung.com>
2014-12-23 16:35:21 +0800

    f2fs: cleanup trace event of f2fs_submit_page_{m,}bio with DECLARE_EVENT_CLASS
    This patch adds missing parameter _type_ for trace_f2fs_submit_page_bio, then
    use DECLARE_EVENT_CLASS/DEFINE_EVENT_CONDITION pair to cleanup some trace event
    code related to f2fs_submit_page_{m,}bio.
    
    Additionally, after we remove redundant code, size of code can be reduced:
       text    data     bss     dec     hex filename
     176787    8712      56  185555   2d4d3 f2fs.ko.org
     174408    8648      56  183112   2cb48 f2fs.ko
    
    Change-Id: Ib3bb01630d2b13191e2e67c6a7de9c49d2a96cd0
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -136,7 +136,7 @@ int f2fs_submit_page_bio(struct f2fs_sb_info *sbi, struct page *page,
 {
 	struct bio *bio;
 
-	trace_f2fs_submit_page_bio(page, fio->blk_addr, fio->rw);
+	trace_f2fs_submit_page_bio(page, fio->blk_addr, fio->rw, fio->type);
 
 	/* Allocate a new bio */
 	bio = __bio_alloc(sbi, fio->blk_addr, 1, is_read_io(fio->rw));
@@ -187,7 +187,7 @@ alloc_new:
 	io->last_block_in_bio = fio->blk_addr;
 
 	up_write(&io->io_rwsem);
-	trace_f2fs_submit_page_mbio(page, fio->rw, fio->type, fio->blk_addr);
+	trace_f2fs_submit_page_mbio(page, fio->blk_addr, fio->rw, fio->type);
 }
 
 /*
