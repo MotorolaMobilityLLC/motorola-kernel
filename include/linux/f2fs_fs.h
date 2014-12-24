a949b78cc08989c070a0a4fe16b0355e0efd885a
Jaegeuk Kim <jaegeuk@kernel.org>
2014-12-23 16:26:31 -0800

    f2fs: fix missing cold bit during recovery
    In do_recover_data, we find and update previous node pages after updating
    its new block addresses.
    After then, we call fill_node_footer without reset field, we erase its
    cold bit so that this new cold node block is written to wrong log area.
    This patch fixes not to miss its old flag.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/f2fs_fs.h
+++ include/linux/f2fs_fs.h
@@ -228,6 +228,8 @@ enum {
 	OFFSET_BIT_SHIFT
 };
 
+#define OFFSET_BIT_MASK		(0x07)	/* (0x01 << OFFSET_BIT_SHIFT) - 1 */
+
 struct node_footer {
 	__le32 nid;		/* node id */
 	__le32 ino;		/* inode nunmber */
