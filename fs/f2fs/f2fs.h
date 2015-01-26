c7285e3f0bff2576afbd27bee18699ca1fc6177e
Chao Yu <chao2.yu@samsung.com>
2015-01-26 20:22:55 +0800

    f2fs: make truncate_inline_date static
    1. make truncate_inline_date static;
    2. remove parameter @from of truncate_inline_date as callers only pass zero.
    
    Signed-off-by: Chao Yu <chao2.yu@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1654,7 +1654,6 @@ int f2fs_read_inline_data(struct inode *, struct page *);
 int f2fs_convert_inline_page(struct dnode_of_data *, struct page *);
 int f2fs_convert_inline_inode(struct inode *);
 int f2fs_write_inline_data(struct inode *, struct page *);
-void truncate_inline_data(struct page *, u64);
 bool recover_inline_data(struct inode *, struct page *);
 struct f2fs_dir_entry *find_in_inline_dir(struct inode *, struct qstr *,
 						struct page **, unsigned int);
