427aa87078c4529d80238e64921f36a62ef96c44
Lianwei Wang <a22439@motorola.com>
2016-07-01 12:15:12 -0500

    f2fs: support case-insensitive from xattr
    The case-insensitive is enabled by setting nocase xattr
    from userspace.
    
    Mot-CRs-fixed: (CR)
    
    Change-Id: I9d43debd28b5c99dc7dd0a7fbe0b16fb52978d3e
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Reviewed-on: http://gerrit.mot.com/892001
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1585,6 +1585,7 @@ void handle_failed_inode(struct inode *);
  * namei.c
  */
 struct dentry *f2fs_get_parent(struct dentry *child);
+void f2fs_set_nocase_dop(struct inode *inode);
 
 /*
  * dir.c
